3e60b59f2cb438fc8fd3b7e34bb63a8902a39c9a
vinyard <antoineleval@gmail.com>
2018-10-17 15:17:28 +0200

    init

---
+++ src/main/java/istic/aoc/m3/active/Canal.java
@@ -0,0 +1,16 @@
+package istic.aoc.m3.active;
+
+/**
+ * @author VinYarD
+ * created : 17/10/2018, 14:41
+ */
+
+
+public class Canal {
+	
+	private long time;
+	
+	public Canal(long time) {
+	
+	}
+}

b1409270f5a5dd46de378c7c5981d6f0817ec8c5
hugo BANNIER <gitlab@penher.bzh>
2018-11-06 09:13:05 +0100

    _Ajout du générateur asynchrone

---
+++ src/main/java/istic/aoc/m3/generator/GeneratorAsync.java
@@ -0,0 +1,16 @@
+package istic.aoc.m3.generator;
+
+import java.util.concurrent.Future;
+
+/**
+ * Le générateur de nombre asynchrone
+ */
+public interface GeneratorAsync {
+
+    /**
+     * Fournit un future d'un long jamais null
+     *
+     * @return Future de long
+     */
+    Future<Long> getValue();
+}
